8eee0931878ae21ab700abba15020e2a51a803ae
Danny Woods <dannywoodz@yahoo.co.uk>
2014-01-01 09:52:31 +0000

    Directory restructure
    First step towards a build layout that works on both Linux and
    OS X.

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :resource-paths ["/home/danny/sfw/lib/jmagick-6.4.0.jar"]
-  :jvm-opts ["-Djava.library.path=/home/danny/sfw/lib/"]
+  :jvm-opts ["-Djava.library.path=resources"]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  :resource-paths ["resources"]
+  :native-path "src/native"
   :main fdi.core)
