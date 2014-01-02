c23205cf4d305f247ec4de544e859bae8c0ab8c0
Danny Woods <dannywoodz@yahoo.co.uk>
2014-01-02 20:55:57 +0000

    Improve cache builder shutdown
    And with less code.  Since all agent tasks are dispatched from the same thread,
    clojure.core/await can be used before shutting down the fingerprint and
    error channels.  This removes the wait-5-seconds hack.

--- src/clj/fdi/cache_builder.clj
+++ src/clj/fdi/cache_builder.clj
@@ -1,9 +1,9 @@
 (ns fdi.cache-builder
-  (:require [clojure.core.async :as async :refer [go chan alts! >!! >!]])
+  (:require [clojure.core.async :as async :refer [go chan alts! >!! >! <!]])
   (:import [java.io File]
            [fdi FileID]))
 
-(defn generate-fingerprint [filename feedback-channel]
+(defn generate-fingerprint [filename success-channel error-channel]
   "Generates a byte-array fingerprint for the image file in the given filename.
 Calls the success-callback with the fingerprint on successor, or error-callback
 with no arguments if an error occurs.  Should never itself throw an error."
@@ -11,54 +11,33 @@ with no arguments if an error occurs.  Should never itself throw an error."
     (let [id (FileID/idString filename)
           fingerprint (FileID/fingerprint filename)
           size (.length (File. filename))]
-      (>!! feedback-channel {:filename filename :id id :fingerprint fingerprint :size size}))
+      (>!! success-channel {:filename filename :id id :fingerprint fingerprint :size size}))
     (catch Exception e
-      (>!! feedback-channel {:filename filename :id :error :fingerprint :error :error e}))))
+      (>!! error-channel {:filename filename :id :error :fingerprint :error :error e}))))
 
-(defmacro after [delay & body]
-  `(do
-     (Thread/sleep ~delay)
-     ~@body))
-
-(defn- agent-generate-fingerprint [state filename feedback-channel]
+(defn- agent-generate-fingerprint [state filename success-channel error-channel]
   "Generates a byte-array fingerprint for the image file in the given filename.
 The first argument, unused, is the state of the agent when the call is made.
 It is otherwise identical to generate-fingerprint, which it calls."
-  (if (= filename :stop)
-    (after 5000 (>!! feedback-channel :stop))
-    (generate-fingerprint filename feedback-channel)))
+  (generate-fingerprint filename success-channel error-channel))
 
 (defn start [filename-channel fingerprint-channel error-channel]
   ;; Starts reading from filename-channel, generating image fingerprints to be
   ;; sent to fingerprint-channel.  error-channel is written to for files
   ;; which cannot be read.
-  (let [cpu-count (.availableProcessors (Runtime/getRuntime))
-        agent-pool (clojure.core/map #(agent %) (range cpu-count))
-        feedback-channel (chan)]
+  (let [thread-count (-> clojure.lang.Agent/pooledExecutor .getCorePoolSize)
+        agent-pool (map #(agent %) (range thread-count))]
     (go
-     (loop [[message channel] (alts! [filename-channel feedback-channel])]
-       (cond
-        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        ;; feedback channel activity
-        (= channel feedback-channel)
-        (cond
-         (= message :stop)
+     (loop [message (<! filename-channel)]
+       (if (= message :stop)
          (do
+           (apply await agent-pool)
            (>! fingerprint-channel :stop)
            (>! error-channel :stop))
-         :default
-         (let [{error :error} message]
-           (if error
-             (>! error-channel message)
-             (>! fingerprint-channel message))
-           (recur (alts! [filename-channel feedback-channel]))))
-        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-        ;; new filename received for caching
-        (= channel filename-channel)
-         (let [agent-index (rem (Math/abs (.hashCode message)) cpu-count)
+         (let [agent-index (rem (Math/abs (.hashCode message)) thread-count)
                selected-agent (nth agent-pool agent-index)]
            (send
             selected-agent
             agent-generate-fingerprint
-            message feedback-channel)
-           (recur (alts! [filename-channel feedback-channel]))))))))
+            message fingerprint-channel error-channel)
+           (recur (<! filename-channel))))))))
