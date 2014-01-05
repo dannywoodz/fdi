5ecb714dfb8cd0215a6fa3ba937f6dabe8ae9fd1
Danny Woods <dannywoodz@yahoo.co.uk>
2014-01-05 17:10:40 +0000

    Don't refer to <!! anymore.

--- src/clj/fdi/analyser.clj
+++ src/clj/fdi/analyser.clj
@@ -1,5 +1,5 @@
 (ns fdi.analyser
-  (:require [clojure.core.async :as async :refer [go <!! <! >!]]))
+  (:require [clojure.core.async :as async :refer [go <! >!]]))
 
 (defn- similar? [{#^bytes first-print :fingerprint fn1 :filename :as fp1}
                  {#^bytes second-print :fingerprint fn2 :filename :as fp2}]
