aa24282e7e90846dbc6331d4eb6ee439b19880b0
Danny Woods <dannywoodz@yahoo.co.uk>
2014-01-06 17:18:31 +0000

    Pull analysis tasks rather than distributing.
    This ensures that the analysis thread pool is optimally used,
    rather than attempting to push tasks to the agents in the pool in
    the hopes that they get an approximately equal amount of work.

--- src/clj/fdi/analyser.clj
+++ src/clj/fdi/analyser.clj
@@ -1,5 +1,6 @@
 (ns fdi.analyser
-  (:require [clojure.core.async :as async :refer [go <! >!]]))
+  (:require [clojure.core.async :as async :refer [go close! chan >!! <!! <! >!]])
+  (:import [java.util.concurrent ArrayBlockingQueue]))
 
 (defn- similar? [{#^bytes first-print :fingerprint fn1 :filename :as fp1}
                  {#^bytes second-print :fingerprint fn2 :filename :as fp2}]
@@ -14,72 +15,29 @@
   (loop [prints all-prints
          duplicates '()]
     (if (empty? prints)
-      (if-not (empty? duplicates)
-        (cons ref-print duplicates)
-        duplicates)
+      duplicates
       (recur (rest prints)
              (if (similar? ref-print (first prints))
                (cons (first prints) duplicates)
                duplicates)))))
 
-(defn- duplicates-of-sub-group [agent-state fingerprints partition-size]
-  (let [before (System/currentTimeMillis)]
-    (loop [prints fingerprints
-           n partition-size
-           results '()]
-      (if (or (zero? n) (empty? prints))
-        (do
-          (println "Agent" agent-state "handled" partition-size "prints in" (- (System/currentTimeMillis) before) "milliseconds")
-          (remove empty? results))
-        (let [ref-print (first prints)]
-          (recur (rest prints)
-                 (dec n)
-                 (cons (duplicates-of ref-print (rest prints))
-                       results)))))))
-
-(defn- task-distribution [partition-count set-size]
-  (let [base-sequence  (take partition-count (drop 2 ((fn rfib [a b]
-                                                        (lazy-seq (cons a (rfib b (+ a b)))))
-                                                      0 1)))]
-    (loop [multiplier 1]
-      (let [scaled-sequence (map #(* % multiplier) base-sequence)]
-        (if (< (apply + scaled-sequence) set-size)
-          (recur (inc multiplier))
-          scaled-sequence)))))
-
-(defn- find-duplicates [fingerprints]
-  ;; Finding duplicates naiively (as per this approach) is an n^2 problem.
-  ;; That time can be halved by noting that 'A is a duplicate of B' implies
-  ;; that B is a duplicate of A.
-  ;; The remaining problem is one of distribution.  A simple partitioning of
-  ;; the search space is uneven: the candidate set for the first agent is
-  ;; the entire input set of prints, but for the last agent it's some fraction
-  ;; of that.
-  ;; This can be slightly spread out by having varying size partitions, which
-  ;; is where #'task-distribution comes in.  Early partitions are smaller than
-  ;; later ones, taking into account that earlier partitions have more of a
-  ;; search space to contend with.
-  (let [thread-count (-> clojure.lang.Agent/pooledExecutor .getCorePoolSize)
-        agent-pool (map #(agent %) (range thread-count))]
-    (loop [agents agent-pool
-           prints fingerprints
-           distribution (task-distribution thread-count (count fingerprints))]
-      (if (empty? agents)
-        (do
-          (apply await agent-pool)
-          (mapcat deref agent-pool))
-        (let [partition-size (first distribution)]
-          (send (first agents)
-                duplicates-of-sub-group
-                prints
-                partition-size)
-          (recur (rest agents)
-                 (drop partition-size prints)
-                 (rest distribution)))))))
+(defn- finder [prints-atom]
+  (loop [[ref-print & prints] (swap! prints-atom rest)
+         duplicates '()]
+    (if (nil? prints)
+      duplicates
+      (let [dups (duplicates-of ref-print prints)]
+        (recur (swap! prints-atom rest)
+               (if (empty? dups)
+                 duplicates
+                 (cons (cons ref-print dups) duplicates)))))))
 
 (defn start [analyser-channel duplicate-handler finished-channel]
   (go
-   (let [prints (<! analyser-channel)
-         duplicates (find-duplicates prints)]
-     (doseq [dup (remove empty? duplicates)] (duplicate-handler dup))
+   (let [fingerprints (atom (<! analyser-channel))
+         thread-count (-> clojure.lang.Agent/pooledExecutor .getCorePoolSize)
+         agent-pool (map (fn [_] (send (agent fingerprints) finder)) (range thread-count))]
+     (apply await agent-pool)
+     (doseq [dup (remove empty? (mapcat deref agent-pool))]
+       (duplicate-handler dup))
      (>! finished-channel :stop))))
