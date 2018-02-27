8e27c5e06934c4912432f2e47fe158413498bcca
Danny Woods <dannywoodz@yahoo.co.uk>
2018-02-27 21:52:59 +0000

    Use pkg-config to get MagickWand details for build

--- build.rs
+++ build.rs
@@ -1,12 +1,21 @@
-extern crate gcc;
+extern crate cc;
+extern crate pkg_config;
 
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=MagickWand-7.Q16");
-    gcc::Build::new()
-        .define("MAGICKCORE_HDRI_ENABLE","0")
-        .define("MAGICKCORE_QUANTUM_DEPTH","16")
-        .include("/usr/include/ImageMagick-7")
-        .flag("-fopenmp")
+    let magick = pkg_config::Config::new().atleast_version("7").probe("MagickWand").unwrap();
+    let hdri_enable = if magick.libs.iter().any(|s| s.find("HDRI").is_some()) {
+        "1"
+    } else {
+        "0"
+    };
+    let mut builder = cc::Build::new();
+    builder
+        .define("MAGICKCORE_HDRI_ENABLE", hdri_enable)
+        .define("MAGICKCORE_QUANTUM_DEPTH","16");
+    for path in &magick.include_paths {
+        builder.include(path);
+    }
+    builder
         .file("src/fdi.c")
         .compile("fdi");
 }
